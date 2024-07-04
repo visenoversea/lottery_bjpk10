1e4ece16ffe37ccd8496dbaabb506637a69c2863
highman <ma.highman@gmail.com>
2024-07-04 22:18:34 +0400

    澳元balance_mop

--- app/home/controller/user.php
+++ app/home/controller/user.php
@@ -35,6 +35,7 @@ class user extends base
             'avatar' => empty($user['avatar']) ? '/img/avatar/0.png' : $user['avatar'],
             'login_ip' => $user['login_ip'],
             'balance' => $user['balance'],
+            'balance_mop' => bcmul($user['balance'],8.05,2),
             'wallet_address' => $user['wallet_address'],
             'create_time' => $user['create_time'],
             'tid'=>$user['tid'],
