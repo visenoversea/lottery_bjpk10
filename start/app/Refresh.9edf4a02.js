b4b995bef80d29dae1efe731bd8c1a9fc51ca551
marco <maxpha010000@outlook.com>
2024-08-05 19:15:55 +0800

    app

---
+++ start/app/Refresh.9edf4a02.js
@@ -0,0 +1,1 @@
+import{q as r,o as a,a as n,C as s}from"./main.06b452f5.js";import{_ as c}from"./index.8c4ec3f4.js";const l=r({beforeRouteEnter(t,e,o){console.log("from.query",e.query),console.log("from.fullPath",e.fullPath),o(()=>{s.replace({path:e.fullPath,query:e.query})})}});function u(t,e,o,f,p,_){return a(),n("div")}const h=c(l,[["render",u]]);export{h as default};
