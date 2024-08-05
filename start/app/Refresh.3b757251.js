ea306b137308930a35a24cec362928a6f66beded
marco <maxpha010000@outlook.com>
2024-08-05 20:10:58 +0800

    app

---
+++ start/app/Refresh.3b757251.js
@@ -0,0 +1,1 @@
+import{q as r,o as a,a as n,C as s}from"./main.8db2bed3.js";import{_ as c}from"./index.d9f35cb3.js";const l=r({beforeRouteEnter(t,e,o){console.log("from.query",e.query),console.log("from.fullPath",e.fullPath),o(()=>{s.replace({path:e.fullPath,query:e.query})})}});function u(t,e,o,f,p,_){return a(),n("div")}const h=c(l,[["render",u]]);export{h as default};
