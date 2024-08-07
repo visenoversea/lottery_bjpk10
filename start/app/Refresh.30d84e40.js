2867f03c7175057680489308ac94734f0160bf3b
marco <maxpha010000@outlook.com>
2024-08-08 06:56:02 +0800

    app

---
+++ start/app/Refresh.30d84e40.js
@@ -0,0 +1,1 @@
+import{q as r,o as a,a as n,C as s}from"./main.3dc1c252.js";import{_ as c}from"./index.431a3ff1.js";const l=r({beforeRouteEnter(t,e,o){console.log("from.query",e.query),console.log("from.fullPath",e.fullPath),o(()=>{s.replace({path:e.fullPath,query:e.query})})}});function u(t,e,o,f,p,_){return a(),n("div")}const h=c(l,[["render",u]]);export{h as default};
