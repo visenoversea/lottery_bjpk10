1e24ce2a00a7913f38ef6e92716022c324219a76
marco <maxpha010000@outlook.com>
2024-08-21 00:41:04 +0800

    start app

---
+++ start/app/HallAdvanced.31930a0b.js
@@ -0,0 +1,1 @@
+import{C as o}from"./CommonHall.75c78401.js";import{o as e,a,d as t}from"./main.01f3864c.js";const c={class:"v_hall g-flex-column n-bg"},_={__name:"HallAdvanced",setup(l){return(r,s)=>(e(),a("div",c,[t(o,{roomId:"3"})]))}};export{_ as default};
---
+++ start/app/HallJunior.9748b50e.js
@@ -0,0 +1,1 @@
+import{C as o}from"./CommonHall.75c78401.js";import{o as e,a,d as r}from"./main.01f3864c.js";const t={class:"v_hall g-flex-column n-bg"},_={__name:"HallJunior",setup(l){return(s,c)=>(e(),a("div",t,[r(o,{roomId:"1"})]))}};export{_ as default};
